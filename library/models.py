25bcdca26dce586e5a8967ce6e8899fd511a2aa5
Joseph <perryjd@cardiff.ac.uk>
2025-03-02 14:04:03 +0000

    DTE/Classifier model development

--- library/models.py
+++ library/models.py
@@ -4,7 +4,7 @@ from .random import split_key
 
 
 class TransformerEncoder(layers.Layer):
-	"""
+	""" Transformer Encoder Block
 	"""
 	
 	# type: (TransformerEncoder, int, int, int, int, float) -> None
@@ -64,11 +64,11 @@ class TransformerEncoder(layers.Layer):
 			gamma_initializer='ones'
 		)
 	
-	# type: (np.ndarray) -> np.ndarray
+	# type: (TransformerEncoder, np.ndarray) -> np.ndarray
 	def call(self, x):
 		
 		skip0 = x
-		z = self.attention(x, x)
+		z = self.attention(x, x) ###! the MultiHeadAttention layer projects x to Q, K internally
 		z = self.drop0(z)
 		z = self.norm0(z+skip0)
 		
@@ -81,15 +81,17 @@ class TransformerEncoder(layers.Layer):
 		return z
 
 
-class PositionEncode(layers.Layer):
-	"""
+class FixedLearnablePositionalEncoding(layers.Layer):
+	""" Fixed size learnable positional encoding
 	"""
 	
+	# type: (FixedLearnablePositionalEncoding, int, int) -> None
 	def __init__(self, key, sequence_length, *args, **kwargs):
-		super(PositionEncode, self).__init__(*args, **kwargs)
+		super(FixedLearnablePositionalEncoding, self).__init__(*args, **kwargs)
 		self.init_key = key
 		self.sequence_length = sequence_length
-
+	
+	# type: (FixedLearnablePositionalEncoding, Tuple[int]) -> None
 	def build(self, input_shape):
 		self.bias = self.add_weight(
 			shape=(self.sequence_length, input_shape[-1]),
@@ -98,11 +100,20 @@ class PositionEncode(layers.Layer):
 			name='bias'
 		)
 	
+	# type: (PositionEncode, np.ndarray) -> np.ndarray
 	def call(self, x):
 		return x + self.bias
 
 
-# type: (int, int, int, int, int, float) -> tf.keras.models.Model
+class PermanentGaussianNoise(layers.GaussianNoise):
+	""" Gaussian Noise layer active at both training and inference time
+	"""
+	
+	# type: (PermanentGaussianNoise, np.ndarray, None) -> np.ndarray
+	def call(self, x, training=None):
+		return super().call(x, training=True)
+
+
 def get_denoising_transformer_encoder(
 		key,
 		n_tokens,
@@ -115,6 +126,12 @@ def get_denoising_transformer_encoder(
 		noise_sd=0.3,
 		name='Denoising-Transformer-Encoder'
 	):
+	""" Instantiate Denoising Transformer Encoder
+	# type: (int, int, int, int, int, int, int, float, float, str) -> tf.keras.models.Model
+	
+	- Sequence length is fixed by position encoding layer
+	- Uses functional interface internally
+	"""
 	
 	# split key
 	noise_key, embed_key, encoder_key = split_key(key, n=3)
@@ -123,17 +140,21 @@ def get_denoising_transformer_encoder(
 	
 	# init input with gaussian perturbation
 	x = tf.keras.Input(shape=(n_tokens, token_dim))
-	z = layers.GaussianNoise(noise_sd, seed=noise_key)(x) # P(-1 <= U(0,0.3) <= +1) = 0.9991419
+	z = PermanentGaussianNoise(noise_sd, seed=noise_key)(x) # P(-1 <= U(0,0.3) <= +1) = 0.9991419
 	
 	# init input embedding
-	z = layers.Dense(
+	input_embedding = layers.Dense(
 		embed_dim,
 		activation=None,
 		kernel_initializer=initializers.GlorotUniform(seed=embed_keys[0]),
 		bias_initializer='zeros',
 		name='input_embedding'
-	)(z)
-	z = PositionEncode(
+	)
+	z = input_embedding(z)
+	###! print([w.shape for w in input_embedding.get_weights()])
+	###! position encoding is not made redundant by embedding biases 
+	###! because embedding biases are applied token-wise
+	z = FixedLearnablePositionalEncoding(
 		embed_keys[1],
 		n_tokens
 	)(z)
@@ -158,7 +179,131 @@ def get_denoising_transformer_encoder(
 	)(z)
 	
 	#finalise model
-	model = models.Model(inputs=x, outputs=z)
-	model.name = name
+	model = models.Model(inputs=x, outputs=z, name=name)
+	model.init_key = key
 	
 	return model
+
+
+def convert_dte_to_classifier(
+		denoising_model,
+		n_classes,
+		key=None,
+		name='Transformer-Encoder-Classifier-from-DTE-backbone'
+	):
+	""" Instantiate Transformer Encoder Classifier from Denoising Transformer Encoder backbone
+	# type: (tf.keras.models.Model, int, int, str) -> tf.keras.models.Model
+	
+	If no key is specified uses the cached DTE init key
+	"""
+	
+	# determine key
+	if key is None:
+		key = denoising_model.init_key
+	
+	# get input
+	x = denoising_model.input
+	
+	# skip over noise layer to input embedding
+	z = denoising_model.layers[2](x)
+	
+	# skip output embedding
+	for layer in denoising_model.layers[3:-1]:
+		z = layer(z)
+	
+	# init classification head
+	yh = layers.Dense(
+		n_classes,
+		activation='softmax',
+		kernel_initializer=initializers.GlorotUniform(seed=key),
+		bias_initializer='zeros',
+		name='classification_head'
+	)(z)
+	
+	# finalise model
+	model = models.Model(inputs=x, outputs=yh, name=name)
+	
+	return model
+
+
+
+
+### Model subclass DTE
+###! build related bug: attempts to trigger build() unsuccessful
+
+# class DenoisingTransformerEncoder(models.Model):
+	# """
+	# """
+	
+	# # type: () ->
+	# def __init__(self,
+			# key,
+			# n_tokens,
+			# token_dim,
+			# embed_dim,
+			# hidden_dim,
+			# n_blocks,
+			# n_heads=8,
+			# dropout=0.1,
+			# noise_sd=0.3,
+			# name='Denoising-Transformer-Encoder',
+			# **kwargs
+		# ):
+		# super(DenoisingTransformerEncoder, self).__init__(name=name, **kwargs)
+		
+		# # split keys
+		# noise_key, embed_key, encoder_key = split_key(key, n=3)
+		# embed_keys = split_key(embed_key, n=3)
+		# encoder_keys = split_key(encoder_key, n=n_blocks)
+		
+		# # store params
+		# self.init_key = key
+		# self.n_tokens = n_tokens
+		# self.token_dim = token_dim
+		# self.embed_dim = embed_dim
+		# self.hidden_dim = hidden_dim
+		# self.n_blocks = n_blocks
+		# self.n_heads = n_heads
+		# self.dropout = dropout
+		# self.noise_sd = noise_sd
+		
+		# # initialise layers
+		# self.gaussian_noise = layers.GaussianNoise(noise_sd, seed=noise_key)
+		
+		# self.input_embedding = layers.Dense(
+			# embed_dim,
+			# activation=None,
+			# kernel_initializer=initializers.GlorotUniform(seed=embed_keys[0]),
+			# bias_initializer='zeros',
+			# name='input_embedding'
+		# )
+		
+		# self.position_encode = PositionEncode(embed_keys[1], n_tokens)
+		
+		# self.transformer_encoders = [
+			# TransformerEncoder(
+				# sub_key,
+				# embed_dim,
+				# hidden_dim,
+				# n_heads=n_heads,
+				# dropout=dropout
+			# )
+			# for sub_key in encoder_keys
+		# ]
+		
+		# self.output_embedding = layers.Dense(
+			# token_dim,
+			# activation='tanh',
+			# kernel_initializer=initializers.GlorotUniform(seed=embed_keys[2]),
+			# bias_initializer='zeros',
+			# name='output_embedding'
+		# )
+	
+	# def call(self, x):
+		# z = self.gaussian_noise(x)
+		# z = self.input_embedding(z)
+		# z = self.position_encode(z)
+		# for encoder in self.transformer_encoders:
+			# z = encoder(z)
+		# z = self.output_embedding(z)
+		# return z
--- train_model_unsupervised.py
+++ train_model_unsupervised.py
@@ -6,7 +6,7 @@ from tensorflow.keras import losses, optimizers, callbacks
 
 from library.random import split_key
 from library.data import reload_cache
-from library.models import get_denoising_transformer_encoder
+from library.models import get_denoising_transformer_encoder, convert_dte_to_classifier
 
 
 ### setup
@@ -18,7 +18,6 @@ print(f'[Elapsed time: {time.time()-T0:.2f}s]')
 # init RNG seeds
 K0 = 999
 K1 = split_key(K0)[1]
-K2 = split_key(K1)[1]
 
 
 ### hyperparameters
@@ -26,7 +25,7 @@ K2 = split_key(K1)[1]
 # architecture
 N_TOKENS = 512
 N_SAMPLES = 96_000 + (96_000 % N_TOKENS)
-EMBED_DIM = 256
+EMBED_DIM = 128
 HIDDEN_DIM = 256
 ENCODER_BLOCKS = 1
 
@@ -34,7 +33,7 @@ ENCODER_BLOCKS = 1
 ETA = 1e-3
 L2_LAM = 0. ###! unimplemented
 BATCH_SIZE = 32
-N_EPOCHS = 30
+N_EPOCHS = 2
 VAL_RATIO = 0.10
 
 # tracing
@@ -49,15 +48,15 @@ assert (N_SAMPLES % N_TOKENS) == 0
 data = reload_cache('data/urbansound8k_mono_24khz_float32.csv')
 
 # pad and tokenize sequences
-data['data'] = data['data'].apply(lambda x : np.array(np.split(np.pad(x, (0,N_SAMPLES-len(x))) if len(x) < N_SAMPLES else x[:N_SAMPLES], N_TOKENS)))
+data['data'] = data['data'].apply(lambda x : np.array(np.split(np.pad(x, (0, N_SAMPLES-len(x))) if len(x) < N_SAMPLES else x[:N_SAMPLES], N_TOKENS)))
 print(data)
 
 # partition data
 train_x = np.array(list(data[(data['fold'] != 1)]['data']))
 test_x = np.array(list(data[(data['fold'] == 1)]['data']))
 
-
-plt.imshow(train_x[np.random.randint(0,len(train_x)-1)])
+# plot sample
+plt.imshow(train_x[np.random.randint(0, len(train_x)-1)])
 plt.show()
 
 # trace
@@ -82,6 +81,11 @@ model = get_denoising_transformer_encoder(
 model.compile(loss=loss_fn, optimizer=optimizer, metrics=['root_mean_squared_error'])
 model.summary()
 
+###! debug
+# model_clf = convert_dte_to_classifier(model, 10)
+# model_clf.summary()
+# import sys;sys.exit()
+
 checkpoint_callback = callbacks.ModelCheckpoint(
 	filepath=f'{model.name}.weights.h5'.replace('-','_').lower(),
 	save_weights_only=True,
@@ -103,7 +107,27 @@ train_history = model.fit(
 	verbose=int(VERBOSE)
 ).history
 print(train_history)
-with open(f'{model.name}_history.pkl'.replace('-','_').lower(), 'wb') as f: pickle.dump(train_history, f)
+
+
+### evaluate model
+
+test_history = model.evaluate(
+	test_x,
+	test_x,
+	batch_size=BATCH_SIZE,
+	verbose=int(VERBOSE),
+	return_dict=True
+)
+print(test_history)
+
+
+### save history
+
+with open(f'{model.name}_history.pkl'.replace('-','_').lower(), 'wb') as f:
+	pickle.dump({
+		'train':train_history,
+		'test':test_history
+	}, f)
 
 
 ### plot history
@@ -111,6 +135,7 @@ with open(f'{model.name}_history.pkl'.replace('-','_').lower(), 'wb') as f: pick
 plt.title('Unsupervised Denoising')
 plt.plot(train_history['loss'], label='train')
 plt.plot(train_history['val_loss'], label='val', c='r')
+plt.scatter([len(train_history['loss'])-1], test_history['loss'], label='test', c='g', marker='x')
 plt.legend()
 plt.xlabel('Epoch')
 plt.ylabel('Loss')
