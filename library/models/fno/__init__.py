0b1bce4a21b2eae773bc4b99ced7eef4de116bb1
Joseph <perryjd@cardiff.ac.uk>
2025-03-17 17:07:14 +0000

    Removed Jax FNO

--- library/models/fno/__init__.py
+++
@@ -1,7 +0,0 @@
-# CREDIT
-# https://github.com/PredictiveIntelligenceLab/TRIPODS_Winter_School_2022/tree/main
-# FNOs.ipynb
-
-from . import data
-from . import layers
-from . import metrics
--- library/models/fno/metrics.py
+++
@@ -1,8 +0,0 @@
-import jax
-import jax.numpy as jnp
-
-
-def mean_squared_error(model, x, y):
-	y_pred = jax.vmap(model)(x)
-	loss = jnp.mean(jnp.square(y_pred - y))
-	return loss
