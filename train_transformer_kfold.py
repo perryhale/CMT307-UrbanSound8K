5b29f938808adcab22cd3dc707fa40422f839177
Joseph <perryjd@cardiff.ac.uk>
2025-03-26 12:09:19 +0000

    Additional metrics in KFold training script

--- train_transformer_kfold.py
+++ train_transformer_kfold.py
@@ -2,6 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.keras import losses, optimizers, callbacks
+from sklearn.metrics import (
+	accuracy_score,
+	confusion_matrix,
+	precision_recall_fscore_support
+)
 
 import time
 import random
@@ -60,21 +65,21 @@ N_CLASSES = 10
 assert (N_SAMPLES % N_TOKENS) == 0
 
 # steps
-N_EPOCHS = 1
+N_EPOCHS = 100
 BATCH_SIZE = 64
+PATIENCE = 10
 
 # learning rate
-ETA = 1e-4
+ETA = 1e-3
 DECAY_RATE = 0.37
 DECAY_FACTOR = 0.1
 
 # explicit regularization
 L2_LAM = 0. ###! unimplemented
-DROPOUT = 0.3
+DROPOUT = 0.4
 
 # data partitions
 VAL_RATIO = 0.10
-#TEST_IDX = 1
 
 # tracing
 VERBOSE = True
@@ -129,11 +134,8 @@ for test_idx in fold_idx:
 		lambda:batch_generator(val_x, val_y, BATCH_SIZE, shuffle=False),
 		output_signature=batch_signature(val_x, val_y)
 	).prefetch(tf.data.experimental.AUTOTUNE)
-
-	test_dataset = tf.data.Dataset.from_generator(
-		lambda:batch_generator(test_x, test_y, BATCH_SIZE, shuffle=False),
-		output_signature=batch_signature(test_x, test_y)
-	).prefetch(tf.data.experimental.AUTOTUNE)
+	
+	test_iterator = batch_generator(test_x, test_y, BATCH_SIZE, shuffle=False)
 	
 	train_steps = len(train_x)//BATCH_SIZE
 	val_steps = len(val_x)//BATCH_SIZE
@@ -176,6 +178,7 @@ for test_idx in fold_idx:
 		mode='min',
 		save_best_only=True
 	)
+	early_stopping_callback = callbacks.EarlyStopping(monitor='val_loss', patience=PATIENCE)
 	
 	print(f'[Elapsed time: {time.time()-T0:.2f}s]')
 	
@@ -188,7 +191,7 @@ for test_idx in fold_idx:
 		steps_per_epoch=train_steps,
 		validation_data=val_dataset,
 		validation_steps=val_steps,
-		callbacks=[checkpoint_callback],
+		callbacks=[checkpoint_callback, early_stopping_callback],
 		verbose=int(VERBOSE)
 	).history
 	
@@ -198,13 +201,37 @@ for test_idx in fold_idx:
 	
 	### evaluate model
 	
+	# reload weights
 	model.load_weights(f'{model.name}.weights.h5')
-	test_history = model.evaluate(
-		test_dataset,
-		steps=test_steps,
-		verbose=int(VERBOSE),
-		return_dict=True
-	)
+	
+	# compute predictions
+	test_y = []
+	test_yh = []
+	for _ in range(test_steps):
+		batch_x, batch_y = next(test_iterator)
+		batch_yh = model.predict(batch_x, verbose=0)
+		test_y.append(batch_y)
+		test_yh.append(batch_yh)
+	
+	test_y = np.concatenate(test_y, axis=0)
+	test_yh = np.concatenate(test_yh, axis=0)
+	
+	# compute scores
+	test_loss = float(model.loss(test_y, test_yh).numpy())
+	test_accuracy = float(accuracy_score(test_y, np.argmax(test_yh, axis=1)))
+	test_cfm = confusion_matrix(test_y, np.argmax(test_yh, axis=1), labels=range(10))
+	test_prfs = precision_recall_fscore_support(test_y, np.argmax(test_yh, axis=1), labels=range(10), average='weighted', zero_division=0.0)
+	test_precision, test_recall, test_f1 = [float(x) for x in test_prfs[:-1]]
+	
+	# record results
+	test_history = {
+		'loss':test_loss,
+		'accuracy':test_accuracy,
+		'confusion':test_cfm,
+		'precision':test_precision,
+		'recall':test_recall,
+		'f1':test_f1
+	}
 	
 	print(test_history)
 	print(f'[Elapsed time: {time.time()-T0:.2f}s]')
