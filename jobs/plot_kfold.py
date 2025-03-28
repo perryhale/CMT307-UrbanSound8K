2e612fb41fa8a539587a697f5b0e6f6d7efb26ff
Joseph <perryjd@cardiff.ac.uk>
2025-03-28 10:01:06 +0000

    Added KFold scores

--- jobs/plot_kfold.py
+++ jobs/plot_kfold.py
@@ -46,10 +46,32 @@ cmap = 'Greys'
 colors = ['Accent', 'Accent_r', 'Blues', 'Blues_r', 'BrBG', 'BrBG_r', 'BuGn', 'BuGn_r', 'BuPu', 'BuPu_r', 'CMRmap', 'CMRmap_r', 'Dark2', 'Dark2_r', 'GnBu', 'GnBu_r', 'Grays', 'Greens', 'Greens_r', 'Greys', 'Greys_r', 'OrRd', 'OrRd_r', 'Oranges', 'Oranges_r', 'PRGn', 'PRGn_r', 'Paired', 'Paired_r', 'Pastel1', 'Pastel1_r', 'Pastel2', 'Pastel2_r', 'PiYG', 'PiYG_r', 'PuBu', 'PuBuGn', 'PuBuGn_r', 'PuBu_r', 'PuOr', 'PuOr_r', 'PuRd', 'PuRd_r', 'Purples', 'Purples_r', 'RdBu', 'RdBu_r', 'RdGy', 'RdGy_r', 'RdPu', 'RdPu_r', 'RdYlBu', 'RdYlBu_r', 'RdYlGn', 'RdYlGn_r', 'Reds', 'Reds_r', 'Set1', 'Set1_r', 'Set2', 'Set2_r', 'Set3', 'Set3_r', 'Spectral', 'Spectral_r', 'Wistia', 'Wistia_r', 'YlGn', 'YlGnBu', 'YlGnBu_r', 'YlGn_r', 'YlOrBr', 'YlOrBr_r', 'YlOrRd', 'YlOrRd_r', 'afmhot', 'afmhot_r', 'autumn', 'autumn_r', 'binary', 'binary_r', 'bone', 'bone_r', 'brg', 'brg_r', 'bwr', 'bwr_r', 'cividis', 'cividis_r', 'cool', 'cool_r', 'coolwarm', 'coolwarm_r', 'copper', 'copper_r', 'cubehelix', 'cubehelix_r', 'flag', 'flag_r', 'gist_earth', 'gist_earth_r', 'gist_gray', 'gist_gray_r', 'gist_grey', 'gist_heat', 'gist_heat_r', 'gist_ncar', 'gist_ncar_r', 'gist_rainbow', 'gist_rainbow_r', 'gist_stern', 'gist_stern_r', 'gist_yarg', 'gist_yarg_r', 'gist_yerg', 'gnuplot', 'gnuplot2', 'gnuplot2_r', 'gnuplot_r', 'gray', 'gray_r', 'grey', 'hot', 'hot_r', 'hsv', 'hsv_r', 'inferno', 'inferno_r', 'jet', 'jet_r', 'magma', 'magma_r', 'nipy_spectral', 'nipy_spectral_r', 'ocean', 'ocean_r', 'pink', 'pink_r', 'plasma', 'plasma_r', 'prism', 'prism_r', 'rainbow', 'rainbow_r', 'seismic', 'seismic_r', 'spring', 'spring_r', 'summer', 'summer_r', 'tab10', 'tab10_r', 'tab20', 'tab20_r', 'tab20b', 'tab20b_r', 'tab20c', 'tab20c_r', 'terrain', 'terrain_r', 'turbo', 'turbo_r', 'twilight', 'twilight_r', 'twilight_shifted', 'twilight_shifted_r', 'viridis', 'viridis_r', 'winter', 'winter_r']
 
 
-### write out mean scores
+### write out scores
 
-mean_df = pd.DataFrame({k:[v] for k,v in zip(['accuracy', 'precision', 'recall', 'f1'], [np.mean(x) for x in [accuracy, precision, recall, f1]])})
-mean_df.to_csv('kfold_mean_scores.csv')
+min_keys = ['min_accuracy', 'min_precision', 'min_recall', 'min_f1']
+mean_keys = ['mean_accuracy', 'mean_precision', 'mean_recall', 'mean_f1']
+max_keys = ['max_accuracy', 'max_precision', 'max_recall', 'max_f1']
+
+min_values = [np.min(x) for x in [accuracy, precision, recall, f1]]
+mean_values = [np.mean(x) for x in [accuracy, precision, recall, f1]]
+max_values = [np.max(x) for x in [accuracy, precision, recall, f1]]
+
+dictionary_keys = [*min_keys, *mean_keys, *max_keys]
+dictionary_values = [*min_values, *mean_values, *max_values]
+
+dictionary = pd.DataFrame({k:[v] for k,v in zip(dictionary_keys, dictionary_values)})
+dictionary.to_csv('kfold_min_mean_max_scores.csv', index=False)
+
+
+dictionary = pd.DataFrame({
+	'fold':range(1, 11),
+	'accuracy':accuracy,
+	'precision':precision,
+	'recall':recall,
+	'f1':f1
+})
+dictionary = dictionary.set_index('fold')
+dictionary = dictionary.to_csv('kfold_all_scores.csv')
 
 
 ### plot metrics
